b31da1922bd5e8d8b1f2a3cf5006db0597749330
AlazarLaz <alazar.yeh@example.com>
2023-06-20 00:41:06 +0300

    Use the new components

--- 0x03-React_component/task_3/dashboard/src/App/App.js
+++ 0x03-React_component/task_3/dashboard/src/App/App.js
@@ -4,69 +4,86 @@ import Header from "../Header/Header";
 import Login from "../Login/Login";
 import Notifications from "../Notifications/Notifications";
 import { getLatestNotification } from "../utils/utils";
-import PropTypes, { func } from 'prop-types'
-import React, { Component } from 'react'
+import PropTypes, { func } from "prop-types";
+import React, { Component } from "react";
 import "./App.css";
-
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import BodySection from "../BodySection/BodySection";
 
 const listCourses = [
-  {id: 1, name: "ES6", credit: 60},
-  {id: 2, name: "Webpack", credit: 20},
-  {id: 3, name: "React’", credit: 40},
-]
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React’", credit: 40 },
+];
 
 const listNotifications = [
   { id: 1, type: "default", value: "New course available" },
   { id: 2, type: "urgent", value: "New resume available" },
   { id: 3, type: "urgent", html: getLatestNotification() },
-]
-
+];
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.handleKeyDown = this.handleKeyDown.bind(this)
+  constructor(props) {
+    super(props);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
-  handleKeyDown(event){
-    if (event.ctrlKey && event.key === 'h') {
-      alert('Logging you out');
+  handleKeyDown(event) {
+    if (event.ctrlKey && event.key === "h") {
+      alert("Logging you out");
       this.props.logOut();
     }
-  };
-  componentDidMount(){
-    document.addEventListener('keydown', this.handleKeyDown);
+  }
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
   }
 
-  componentWillUnmount(){
-    document.removeEventListener('keydown', this.handleKeyDown);
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
   }
   render() {
     return (
-    <React.Fragment>
-      <div className="Notify">
-        <Notifications listNotifications={listNotifications} />
-      </div>
-      <div className="App">
-        <Header />
-        <body>{this.props.isLoggedIn ? <Login /> : <CourseList listCourses={ listCourses } />}</body>
-        <Footer />
-      </div>
-    </React.Fragment>
-    )
+      <React.Fragment>
+        <div className="Notify">
+          <Notifications listNotifications={listNotifications} />
+        </div>
+        <div className="App">
+          <Header />
+          <body>
+            {this.props.isLoggedIn ? (
+              <BodySectionWithMarginBottom
+                title="Log in to continue"
+                children={<Login />}
+              />
+            ) : (
+              <BodySectionWithMarginBottom
+                title="Course list"
+                children={<CourseList listCourses={listCourses} />}
+              />
+            )}
+          </body>
+          <BodySection title="News from the School">
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis at tempora odio, necessitatibus repudiandae reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo ipsa
+              iste vero dolor voluptates.
+            </p>
+          </BodySection>
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
 App.defaultProps = {
   isLoggedIn: false,
-  logOut: ()=>{
-    return
-  }
+  logOut: () => {
+    return;
+  },
 };
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func
+  logOut: PropTypes.func,
 };
-
 
 export default App;
---
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import BodySection from './BodySection';
+import './BodySectionWithMarginBottom.css';
+import PropTypes from "prop-types";
+
+export default class BodySectionWithMarginBottom extends Component {
+  render() {
+    return (
+      <div className='bodySectionWithMargin'><BodySection {...this.props}/></div>
+    )
+  }
+}
+BodySectionWithMarginBottom.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  };
+  
